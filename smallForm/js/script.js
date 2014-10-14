3b8535fcb2a5634e356d7be8c422cf291198dc36
Thomas Chabaud <tchabaud@laposte.net>
2014-10-14 09:12:27 +0100

    Renaming title.

---
+++ smallForm/js/script.js
@@ -0,0 +1,33 @@
+String.prototype.isEmpty = function () {
+    return (this.length === 0 || !this.trim());
+};
+
+var labels = document.getElementsByTagName('label');
+for (var i = 0; i < labels.length; i++) {
+    if (labels[i].htmlFor != '') {
+        var elem = document.getElementById(labels[i].htmlFor);
+        if (elem) {
+            elem.label = labels[i];
+        }
+    }
+}
+
+var btn = document.getElementById('submit');
+btn.addEventListener('click', validate);
+
+function validate() {
+    var lInputs = document.querySelectorAll("input[type=text]");
+    for (var i = 0; i < lInputs.length; i++) {
+        check(lInputs[i], ' error');
+    }
+}
+
+function check(pElement, pClass) {
+    var label = pElement.label;
+    if (pElement.value.isEmpty()) {
+        label.className = label.className + pClass;
+    }
+    else if (label.className.indexOf(pClass) > -1) {
+        label.className = label.className.replace(pClass, '');
+    }
+}
---
+++ testCanvas/js/script.js
@@ -0,0 +1,55 @@
+/*global $:false */
+$(document).ready(
+    function () {
+        'use strict';
+        var lCanvas,
+            lCtx,
+            i,
+            x,
+            y,
+            canvas,
+            shift,
+            width,
+            height;
+        lCanvas = document.getElementById('drawHere');
+        lCtx = lCanvas.getContext('2d');
+        i = 0;
+        x = 0;
+        y = 0;
+        shift = 10;
+        height = lCanvas.height;
+        width = lCanvas.width;
+        try {
+
+            lCtx.beginPath();
+            for (x = 0; x < width; x = x + shift) {
+                if (x === shift) {
+                    lCtx.font = 'bold 20px sans-serif';
+                    lCtx.fillText('x', shift, width / 2);
+                }
+
+                lCtx.moveTo(x, 0);
+                lCtx.lineTo(x, width);
+            }
+            lCtx.strokeStyle = '#00ff00';
+            lCtx.stroke();
+            lCtx.closePath()
+
+            lCtx.beginPath();
+            for (y = 0; y < height; y = y + shift) {
+                if (y === shift) {
+                    lCtx.font = 'bold 20px sans-serif';
+                    lCtx.fillText('y', height / 2, shift);
+                }
+
+                lCtx.moveTo(0, y);
+                lCtx.lineTo(height, y);
+            }
+            lCtx.strokeStyle = '#ff0000';
+            lCtx.stroke();
+            lCtx.closePath();
+        } catch (err) {
+            alert(err);
+        }
+    });
+
